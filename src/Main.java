6fa770d4bd75f2b5057a0057312eadccecc1a8f3
user <akumusaeva01@gmail.com>
2023-05-26 10:57:51 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         /**Homework-10 LMS*/
-        Circle circle=new Circle(4);
+       /* Circle circle=new Circle(4);
         System.out.println("Площадь круга равна "+Circle.area());
-        System.out.println("Длина окружности равна "+Circle.circumference());
+        System.out.println("Длина окружности равна "+Circle.circumference());*/
+        
+
+
 
 
     }
